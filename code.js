b38f5bf894fdaf2f500d0da46b95f660221c4f53
Donovan Knight <99293948+dkthedeveloper@users.noreply.github.com>
2022-12-16 09:30:05 -0800

    Got rid of borders on main and form sections, and changed hover color on task list items

--- code.js
+++ code.js
@@ -1,73 +1,47 @@
-let submitButton = document.getElementById("submit")
-let clearButton = document.getElementById("clear")
-let list = document.getElementById("tasklist")
-let nameInput = document.getElementById("name")
-let taskInput = document.getElementById("task")
-let listHeader = document.getElementById("listhead")
-let userName = ""
-let task = ""
-let form = document.getElementById("form")
-let taskList = []
-let items = 0
-//list.innerHTML = ""
-
-
-
-
+let submitButton = document.getElementById("submit");
+let clearButton = document.getElementById("clear");
+let list = document.getElementById("tasklist");
+let nameInput = document.getElementById("name");
+let taskInput = document.getElementById("task");
+let listHeader = document.getElementById("listhead");
+let userName = "";
+let task = "";
+let form = document.getElementById("form");
+let taskList = [];
+let items = 0;
 
 submitButton.addEventListener("click", handleSubmit);
 clearButton.addEventListener("click", clearForm);
 
-
-
-
-
-
-
-function handleSubmit (event)  {
-    event.preventDefault();
-    if (taskInput.value === "" || nameInput.value === "") {
-        alert('This cannot be empty')
-        return false;
-     }  else if (taskInput.value.length > 12) {
-        alert('Please limit task to 12 characters');
-        return false
-     }
-    else    {
-    userName = nameInput.value
-    task = taskInput.value
-    taskList.push(task)
-    listHeader.innerHTML = `${userName}'s Tasks`
-    let li = document.createElement("li")
+function handleSubmit(event) {
+  event.preventDefault();
+  if (taskInput.value === "" || nameInput.value === "") {
+    alert("This cannot be empty");
+    return false;
+  } else if (taskInput.value.length > 12) {
+    alert("Please limit task to 12 characters");
+    return false;
+  } else {
+    userName = nameInput.value;
+    task = taskInput.value;
+    taskList.push(task);
+    listHeader.innerHTML = `${userName}'s Tasks`;
+    let li = document.createElement("li");
     li.innerHTML = `${taskList[items]}<button onclick="this.parentNode.remove()" id="done">X</button></li>`;
-    list.appendChild(li)
-    //let deleteTask = document.getElementById("done")
-    //deleteTask.addEventListener("click", deleteChild)
-    //taskList.forEach((o, items) => o.id = items);
-      
-    items++
-    
-    taskInput.value = taskInput.defaultValue
-    taskInput.focus()
-}
+    list.appendChild(li);
+
+    items++;
+
+    taskInput.value = taskInput.defaultValue;
+    taskInput.focus();
+  }
 }
 
-function clearForm ()   {
-    list.innerHTML = ""
-    items = null
+function clearForm() {
+  list.innerHTML = "";
+  items = null;
 }
 
-
-
-function deleteChild (e) {
-    //e.target.parentNode.remove()
-    //let item = document.getElementById(nameInput.)
-    list.removeChild(list.lastElementChild)
+function deleteChild(e) {
+  list.removeChild(list.lastElementChild);
 }
-
-
-/*
-onclick="deleteChild()"
-*/
-
-
